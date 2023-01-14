0b43e555c445aa9f0f53b8a4f7245db98295584c
Ari Ahmed <59537867+areeahmed@users.noreply.github.com>
2023-01-14 18:20:53 +0300

    getting ready and adding curved drawer package

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,10 +5,9 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:curved_drawer/src/myapp.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:curved_drawer/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
